1198ac8263660de594407dfa6e6d4923dea64ae4
Edcoluag <155853509+Edcoluag@users.noreply.github.com>
2024-01-08 23:27:43 -0500

    Update bdagenda.sql

--- bdagenda.sql
+++ bdagenda.sql
@@ -4,8 +4,7 @@ USE BASEDATOS;
 DROP TABLE IF EXISTS distrito;
 CREATE TABLE distrito
  (codigodis                  VARCHAR(3)  NOT NULL,
-  nombre                     VARCHAR(45) NOT NULL,
-  CONSTRAINT distrito_codigodis_pk PRIMARY KEY (codigodis)
+  nombre                     VARCHAR(45) NOT NULL
  );
 
 DROP TABLE IF EXISTS agenda;
@@ -18,33 +17,45 @@ CREATE TABLE agenda
   telefmovi                  VARCHAR(12),
   email                      VARCHAR(30),
   fbc                        VARCHAR(30),
-  distrito                   VARCHAR(30),
-  CONSTRAINT agenda_codigoper_pk PRIMARY KEY (codigoper),
-  CONSTRAINT agenda_distrito_fk FOREIGN KEY (distrito) REFERENCES distrito(codigodis)
+  distrito                   VARCHAR(30)null
  );
+
+-- Agregamos las restricciones de PRIMARY KEY y FOREIGN KEY después de la creación de las tablas
+-- utilizando ALTER TABLE
+
+-- Para la tabla distrito
+ALTER TABLE distrito ADD CONSTRAINT distrito_codigodis_pk PRIMARY KEY (codigodis);
+
+-- Para la tabla agenda
+ALTER TABLE agenda ADD CONSTRAINT agenda_codigoper_pk PRIMARY KEY (codigoper);
+ALTER TABLE agenda ADD CONSTRAINT agenda_distrito_fk FOREIGN KEY (distrito) REFERENCES distrito(codigodis);
+
+-- Inserciones de datos
+INSERT INTO distrito VALUES('1','Chimbote');
+INSERT INTO distrito VALUES('2','NuevoChimbote');
+
 
 INSERT INTO agenda VALUES ('1', 'carlos', 'serna','av. palmeras 145',
 '043524875', '942587568','carlos@hotmail.com', 'carlos serna','1');
 INSERT INTO agenda VALUES ('2', 'carmen', 'figueroa','av. alamos f-5',
 '043154784', '943124578','carmen@hotmail.com', 'carmen figeroa','1');
 INSERT INTO agenda VALUES ('3', 'juan', 'perez','av.pacaes',
-'043758694', '943582674','juan@hotmail.com', 'juan perez');
+'043758694', '943582674','juan@hotmail.com', 'juan perez','1');
 INSERT INTO agenda VALUES ('4', 'jean', 'ramirez','av.pardo 580',
-'043587469', '941748596','jean@hotmail.com', 'jean ramirez');
+'043587469', '941748596','jean@hotmail.com', 'jean ramirez','1');
 INSERT INTO agenda VALUES ('5', 'melisa', 'cavero','av.huaraz 220',
-'043152648', '942121415','melisa@hotmail.com', 'melisa cavero');
+'043152648', '942121415','melisa@hotmail.com', 'melisa cavero','1');
 INSERT INTO agenda VALUES ('6', 'klaus', 'arteaga','av.lima 145',
-'048757686', '943332524','klaus@hotmail.com', 'klaus arteaga');
+'048757686', '943332524','klaus@hotmail.com', 'klaus arteaga','1');
 INSERT INTO agenda VALUES ('7', 'octavio', 'baldarrago','av.galvez 874',
-'043584736', '941747485','octavio585@hotmail.com', 'octavio baldarrago');
+'043584736', '941747485','octavio585@hotmail.com', 'octavio baldarrago','1');
 INSERT INTO agenda VALUES ('8', 'angela', 'suarez','av.pardo 1894',
-'043252526', '941741212','angela_humilde@hotmail.com', 'angela suarez');
+'043252526', '941741212','angela_humilde@hotmail.com', 'angela suarez','1');
 INSERT INTO agenda VALUES ('9', 'antonhy', 'orbegozo','av.palmeras 456',
-'043151919', '942321212','antonhyelgozo@hotmail.com', 'antonhy orbegozo');
+'043151919', '942321212','antonhyelgozo@hotmail.com', 'antonhy orbegozo','1');
 INSERT INTO agenda VALUES ('10', 'giancarlo', 'sarrin','av.huaraz 123',
-'043411742', '9744105472','giancarlosarrines@hotmail.com', 'giancarlo sarrin');
+'043411742', '9744105472','giancarlosarrines@hotmail.com', 'giancarlo sarrin','1');
 
-INSERT INTO distrito VALUES('1','Chimbote');
-INSERT INTO distrito VALUES('2','NuevoChimbote');
+
 
 COMMIT;
